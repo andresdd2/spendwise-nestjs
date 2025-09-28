b03e4842eb633ed4a4da5ed092d0a2a2e00f6939
Andrew <locogb26@gmail.com>
2025-09-28 10:50:21 -0500

    Add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,9 +10,18 @@ async function bootstrap() {
     new ValidationPipe({
       whitelist: true,
       transform: true,
-      
     })
-  )
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('SpendWise')
+    .setDescription('SpendWise API')
+    .setVersion('1.0')
+    .addTag('spendwise')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
 
   await app.listen(process.env.PORT ?? 3000);
 }
